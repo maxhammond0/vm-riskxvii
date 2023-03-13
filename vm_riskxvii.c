9deac2dcfbd4924d5f9792773e16516d78460701
Max Hammond <max.hammond21@gmail.com>
2023-03-13 21:54:45 +1100

    error messages and hexdump

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -8,13 +8,16 @@ int main (int argc, char const *argv[])
     char buff[OFFSET] = "";
     int read, address = 0, i;
     if (argc != 2) {
-        printf("Incorrect usage\n");
+        printf("Incorrect argument count\n");
+        printf("Should be ./vm_riskxvii <file_path>\n");
+        printf("exiting...\n");
         return 1;
     }
     FILE *fp = fopen (argv[1], "rb");
 
     if (!fp) {
-        perror ("fopen");
+        printf("File could not be opened\n");
+        printf("exiting...\n");
         return 2;
     }
 
@@ -23,7 +26,7 @@ int main (int argc, char const *argv[])
         address += OFFSET;
 
         for (i = 0; i < OFFSET; i++)    /* print hex values */
-            if (i >= read || buff[i] == 0)
+            if (i >= read )
                 printf("   ");
             else
                 printf("%02hhx ", (unsigned char)buff[i]);
