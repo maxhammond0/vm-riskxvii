d9c8abfe7c94f026cc2d9c2c00332c7686be3bc4
Max Hammond <max.hammond21@gmail.com>
2023-03-13 21:12:34 +1100

    proper hexdump

---
+++ test.py
@@ -0,0 +1,5 @@
+file = 'examples/printing_h/printing_h.mi'
+with open(file, 'rb') as f:
+    lines = f.readlines()
+
+print(lines)
--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -1,40 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int read_file(const char* filename) {
+#define OFFSET 4
 
-    FILE *file = fopen(filename, "rb");
-    char ch;
-
-    if (file == NULL) {
-        printf("File could not be opened\nExiting...\n");
-        exit(2);
-    }
-    do {
-        ch = fgetc(file);
-        printf("%x", ch);
- 
-        // Checking if character is not EOF.
-        // If it is EOF stop reading.
-    } while (ch != EOF);
-    printf("\n");
-
-    fclose(file);
-
-    return 0;
-}
-
-int main(int argc, char **argv) {
-
+int main (int argc, char const *argv[]) 
+{
+    char buff[OFFSET] = "";
+    int read, address = 0, i;
     if (argc != 2) {
-        printf("Wrong number of arguments\n");
-        printf("First argument should be path to binary file\n");
-        printf("Exiting...\n");
+        printf("Incorrect usage\n");
         return 1;
     }
+    FILE *fp = fopen (argv[1], "rb");
 
-    char *file = argv[1];
-    read_file(file);
+    if (!fp) {
+        perror ("fopen");
+        return 2;
+    }
+
+    while ((read = fread(buff, 1, sizeof buff, fp)) > 0) {
+        printf("%08x ", address);
+        address += OFFSET;
+
+        for (i = 0; i < OFFSET; i++)    /* print hex values */
+            if (i >= read || buff[i] == 0)
+                printf("   ");
+            else
+                printf("%02hhx ", (unsigned char)buff[i]);
+
+        putchar ('\n'); /* use putchar to output single character */
+    }
+
+    fclose (fp);
 
     return 0;
 }
