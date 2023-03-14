d7f29f0e0037ce8a80ed6d86286ef40f674f41b0
Max Hammond <max.hammond21@gmail.com>
2023-03-15 09:53:54 +1100

    instruction reading

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -1,51 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdint.h>
-#include <string.h>
 
-#define OFFSET 4
+void convert_hex_to_bin() {
 
-void handle_file(const char *filename) {
-    FILE *fp = fopen(filename, "rb");
-    if (!fp) {
-        printf("File could not be opened\n");
-        printf("exiting...\n");
-        exit(2);
-    }
-    char buff[OFFSET] = "";
-    int read, address = 0;
-
-    while ((read = fread(buff, 1, sizeof buff, fp)) > 0) {
-        printf("%03x ", address);
-        address += OFFSET;
-
-        uint8_t op = 0u;
-        for (int i = OFFSET; i >= 0; i--) {    /* print hex values */
-            if (i < read) {
-                op |= (unsigned char)buff[i] << (4-i)*8;
-                printf("%02hhx", (unsigned char)buff[i]);
-                // printf(" %d ", (unsigned char)buff[i]);
-            }
-        }
-        printf(" %x", op);
-
-        putchar ('\n'); /* use putchar to output single character */
-    }
-
-    fclose(fp);
 }
 
-
-int main (int argc, char const *argv[]) 
+int main( int argc, char *argv[])
 {
-    if (argc != 2) {
-        printf("Incorrect argument count\n");
-        printf("Should be ./vm_riskxvii <file_path>\n");
-        printf("exiting...\n");
+    int fd;
+    int retval;
+    int each = 0;
+    unsigned char buf[16] = {0};
+    unsigned char opcode[4] = {0};
+    unsigned char arg1[4] = {0};
+    unsigned char arg2[4] = {0};
+    unsigned char arg3[4] = {0};
+
+    // Wrong number of cmd line arguments
+    if ( argc != 2) {
+        printf("Wrong number of command line arguments\n");
+        printf("Exiting...\n");
         return 1;
     }
 
-    handle_file(argv[1]);
+    // File can't be read
+    if ((fd = open(argv[1], O_RDONLY)) < 0) {
+        printf("Could not open file\n");
+        printf("Exiting...\n");
+        return 2;
+    }
 
+    // Read until EOF
+    while ( ( retval = read ( fd, &buf, 4)) > 0) {
+        // Read 4 bytes at a time
+        if ( retval == 4) {
+            for ( each = 0; each < 4; each++) {
+                arg3[each] = buf[each];
+                arg2[each] = buf[each + 1];
+                arg1[each] = buf[each + 2];
+                opcode[each] = buf[each + 3];
+            }
+            // printf("%02x%02x%02x%02x", (unsigned int)*opcode,
+            //        (unsigned int)*arg1,
+            //        (unsigned int)*arg2,
+            //        (unsigned int)*arg3);
+            uint32_t op = 0u;
+            op |= (unsigned int)*opcode << 24;      // 0xAA000000
+            op |= (unsigned int)*arg1 << 16;      // 0xaaBB0000
+            op |= (unsigned int)*arg2 << 8;       // 0xaabbCC00
+            op |= (unsigned int)*arg3;            // 0xaabbccDD
+            printf("%08x\n", op);
+        }
+    }
+
+    close(fd);
     return 0;
 }
