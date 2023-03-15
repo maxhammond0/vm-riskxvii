793af09f576c0c29c58ba4a24d65ece5d85ab5df
Max Hammond <max.hammond21@gmail.com>
2023-03-15 14:43:00 +1100

    plan for handling input

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -5,12 +5,10 @@
 #include <fcntl.h>
 #include <stdint.h>
 
-void convert_hex_to_bin(uint32_t hex) {
-    // char binary[33] = {0};
-}
+typedef uint32_t INSTRUCTION;
 
-int main( int argc, char *argv[])
-{
+void read_file(char *filepath, INSTRUCTION *instructions) {
+    // Reads file and loads instructions into the instructions array
     int fd;
     int retval;
     int each = 0;
@@ -20,20 +18,15 @@ int main( int argc, char *argv[])
     unsigned char arg2[4] = {0};
     unsigned char arg3[4] = {0};
 
-    // Wrong number of cmd line arguments
-    if ( argc != 2) {
-        printf("Wrong number of command line arguments\n");
-        printf("Exiting...\n");
-        return 1;
-    }
 
     // File can't be read
-    if ((fd = open(argv[1], O_RDONLY)) < 0) {
+    if ((fd = open(filepath, O_RDONLY)) < 0) {
         printf("Could not open file\n");
         printf("Exiting...\n");
-        return 2;
+        exit(2);
     }
 
+    int pc = 0;
     // Read until EOF
     while ( ( retval = read ( fd, &buf, 4)) > 0) {
         // Read 4 bytes at a time
@@ -44,20 +37,56 @@ int main( int argc, char *argv[])
                 arg1[each] = buf[each + 2];
                 opcode[each] = buf[each + 3];
             }
-            // printf("%02x%02x%02x%02x", (unsigned int)*opcode,
-            //        (unsigned int)*arg1,
-            //        (unsigned int)*arg2,
-            //        (unsigned int)*arg3);
-            uint32_t op = 0u;
+
+            INSTRUCTION op = 0u;
             op |= (unsigned int)*opcode << 24;      // 0xAA000000
             op |= (unsigned int)*arg1 << 16;      // 0xaaBB0000
             op |= (unsigned int)*arg2 << 8;       // 0xaabbCC00
             op |= (unsigned int)*arg3;            // 0xaabbccDD
-            printf("%08x\n", op);
-            // convert_hex_to_bin(op);
+            instructions[pc] = op;
+            pc++;
         }
     }
 
-    close(fd);
+}
+
+void mask(int i, int j) {
+    // Return 
+}
+
+void get_op() {
+
+}
+
+void get_func3() {
+
+}
+
+void get_func7() {
+
+}
+
+void process_instruction() {
+
+}
+
+int main( int argc, char *argv[]) {
+
+    // Wrong number of cmd line arguments
+    if ( argc != 2) {
+        printf("Wrong number of command line arguments\n");
+        printf("Exiting...\n");
+        return 1;
+    }
+
+    INSTRUCTION instructions[1024] = { 0 };
+
+    read_file(argv[1], instructions);
+
+    // Print out all instructions (for debugging)
+    // for (int i = 0; i < 1024; i++) {
+    //     printf("%08x\n", instructions[i]);
+    // }
+
     return 0;
 }
