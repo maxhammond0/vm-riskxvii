cefc3866b9bac19f834cda51512b5114d7b99c2e
Max Hammond <max.hammond21@gmail.com>
2023-03-23 15:21:35 +1100

    debug

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -245,7 +245,7 @@ void s(INSTRUCTION instruction, uint8_t data_mem[DATA_MEM_SIZE]) {
         return;
     }
 
-    // addy = addy - 1024;
+    addy = addy/4;
     if (addy < 0 || addy > DATA_MEM_SIZE) {
         printf("\n%d\n", addy);
         printf("address out of bounds\n!\n");
@@ -323,7 +323,7 @@ void memory_load(INSTRUCTION instruction,
             printf("addy > 0x7ff: %x", addy);
         }
 
-        // addy = addy - 1024;
+        addy = addy/4;
         if (addy < 0 || addy > DATA_MEM_SIZE) {
             printf("\n%d\n", addy);
             printf("address out of bounds\n!\n!\n!\n!\n!\n");
