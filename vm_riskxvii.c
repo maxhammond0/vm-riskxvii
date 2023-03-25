9447d0736ae5169e6149b24a56eb5afc0a1a8574
Max Hammond <max.hammond21@gmail.com>
2023-03-25 13:08:36 +1100

    submit

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -547,24 +547,25 @@ int main( int argc, char *argv[]) {
     get_instructions(argv[1], instructions, data_mem);
     
     // print(instructions)
-    for (int i = 0; i < INST_MEM_SIZE/4; i+=4) {
-        printf("%x %x %x %x\n", instructions[i*4],
-               instructions[(i*4)+1],
-               instructions[(i*4)+2],
-               instructions[(i*4)+3]);
-    }
+    // for (int i = 0; i < INST_MEM_SIZE; i+=4) {
+    //     printf("%x %x %x %x\n", instructions[i*4],
+    //            instructions[(i*4)+1],
+    //            instructions[(i*4)+2],
+    //            instructions[(i*4)+3]);
+    // }
 
     // Run program
-    // for ( ; pc < INST_MEM_SIZE/4; pc+=4) {
-    //     if (debug) printf("pc: %04d, ", pc);
-    //     process_instruction(instructions,
-    //                         instructions[pc],
-    //                         instructions[pc+1],
-    //                         instructions[pc+2],
-    //                         instructions[pc+3],
-    //                         data_mem);
-    //     if (debug) printf("\n");
-    // }
+    for ( ; pc < INST_MEM_SIZE/4; pc+=4) {
+        if (debug) printf("pc: %04d, ", pc);
+        process_instruction(instructions,
+                            instructions[pc],
+                            instructions[pc+1],
+                            instructions[pc+2],
+                            instructions[pc+3],
+                            data_mem);
+        if (debug) printf("\n");
+    }
+    printf("CPU Halt Requested\n");
 
     return 0;
 }
