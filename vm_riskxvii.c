be8d19d724ae6241fdf7fff35acdaa75c87b2b41
Max Hammond <max.hammond21@gmail.com>
2023-03-26 11:40:23 +1100

    debug

--- vm_riskxvii.c
+++ vm_riskxvii.c
@@ -212,6 +212,7 @@ void i(INSTRUCTION instruction,
                 uint32_t byte = location[addy];
                 if (byte >> 7 == 1) {
                     byte = byte | 0b11111111111111111111111110000000;
+                    print_binary(byte);
                 }
                 reg[rd] = byte;
             } else if (func3 == 0b001) {  // lh
@@ -223,6 +224,7 @@ void i(INSTRUCTION instruction,
                 uint32_t byte2 = location[addy] | location[addy+1] << 8;
                 if (byte2 >> 15 == 1) {
                     byte2 = byte2 | 0b11111111111111111000000000000000;
+                    print_binary(byte2);
                 }
                 reg[rd] = byte2;
             } else if (func3 == 0b010) {  // lw
